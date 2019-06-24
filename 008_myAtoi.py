cb262836f9be6fc4d4b2af90a3da43911509b041
ZhangNing777 <30955643+ZhangNing777@users.noreply.github.com>
2019-06-24 23:48:46 +0800

    Update 008_myAtoi.py

--- 008_myAtoi.py
+++ 008_myAtoi.py
@@ -55,7 +55,6 @@ class Solution:
         a = 0
         b = 0
         n = len(str)
-        x = y = n
         
         for i in range(0,n):
             if str[i] == " ":
@@ -64,20 +63,15 @@ class Solution:
             elif str[i] in ["-","+"]:
                 if b == 1:
                     break
+                else:
+                    b = 1
                 if str[i] == "-": 
                     index = -1
-                    b = 1
-                if str[i] == "+":
-                    index = 1
-                    b = 1
             elif str[i] in ["0","1","2","3","4","5","6","7","8","9"]:
                 a = a*10 + int(str[i])
                 b = 1
             else:
-                if b == 0:
-                    return 0
-                if b == 1:
-                    break
+                break
                 
         
         
@@ -92,4 +86,4 @@ class Solution:
         return a
 
 a = Solution()
-print(a.myAtoi("  words and 987"))
+print(a.myAtoi(" words and 987"))
