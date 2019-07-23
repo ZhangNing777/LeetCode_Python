d5d562001f62a7775c3c16eb5096485ece936f36
ZhangNing777 <30955643+ZhangNing777@users.noreply.github.com>
2019-07-23 21:30:59 +0800

    Create 219_containsNearbyDuplicate.py

---
+++ 219_containsNearbyDuplicate.py
@@ -0,0 +1,36 @@
+'''
+给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums [i] = nums [j]，并且 i 和 j 的差的绝对值最大为 k。
+
+示例 1:
+
+输入: nums = [1,2,3,1], k = 3
+输出: true
+示例 2:
+
+输入: nums = [1,0,1,1], k = 1
+输出: true
+示例 3:
+
+输入: nums = [1,2,3,1,2,3], k = 2
+输出: false
+'''
+
+class Solution:
+    def containsNearbyDuplicate(self, nums:[int], k: int) -> bool:
+        l = len(nums)
+        
+        d = {}
+        
+        for i in range(0,l):
+            if d.get(nums[i],None) == None:
+                d[nums[i]] = i
+            else:
+                if i - d.get(nums[i],None)<= k:
+                    return True
+                else:
+                    d[nums[i]] = i
+                        
+        return False
+
+a=Solution()
+print(a.containsNearbyDuplicate([1,2,3,1,2,3],2))
