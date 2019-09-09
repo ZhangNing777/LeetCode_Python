fa471a4c53e0b5dccd68848f54feea93202d6164
ZhangNing777 <30955643+ZhangNing777@users.noreply.github.com>
2019-09-09 16:00:07 +0800

    Create 572_isSubtree.py

---
+++ 572_isSubtree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
+        if not t:
+            return True
+        if not s:
+            return False
+        return self.isSame(s,t) or self.isSubtree(s.left,t) or self.isSubtree(s.right,t)
+        
+    def isSame(self,p,q):
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        return p.val==q.val and self.isSame(p.left,q.left) and self.isSame(p.right,q.right)
